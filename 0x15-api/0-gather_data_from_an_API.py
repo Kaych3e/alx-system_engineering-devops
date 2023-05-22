58399463bc99bebc8d736861f6fb24da3987f58f
root <nkaduba@gmail.com>
2023-05-22 15:02:16 +0000

    API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script using REST API that returns info about a TODO list progress
+""" 
+import requests
+import sys
+
+if __name__ == "__main__":
+    """URL without path"""
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = int(sys.argv[1])
+    user_endpoint = "{}/users/{}".format(url, user_id)
+    name = requests.get(user_endpoint).json().get('name')
+    task_endpoint = "{}/todos".format(url)
+    tasks = request.get(task_endpoint).json()
+    all_tasks = [task for task in tasks if task.get("userId") == user_id]
+    done_tasks = [task for task in all_tasks if task.get("completed")]
+
+    print("Employee {} is done with tasks({}/{}):"
+            .format(name, len(done_tasks), len(all_tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
