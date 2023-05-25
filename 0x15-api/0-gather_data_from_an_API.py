fa43a6bbf6bee4828085994e59e6701e7a88f211
root <nkaduba@gmail.com>
2023-05-25 01:45:51 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,12 +7,12 @@ import sys
 
 if __name__ == "__main__":
     """URL without path"""
-    url = "https://jsonplaceholder.typicode.com"
+    url = "https://jsonplaceholder.typicode.com/"
     user_id = int(sys.argv[1])
     user_endpoint = "{}/users/{}".format(url, user_id)
     name = requests.get(user_endpoint).json().get('name')
     task_endpoint = "{}/todos".format(url)
-    tasks = request.get(task_endpoint).json()
+    tasks = requests.get(task_endpoint).json()
     all_tasks = [task for task in tasks if task.get("userId") == user_id]
     done_tasks = [task for task in all_tasks if task.get("completed")]
 
