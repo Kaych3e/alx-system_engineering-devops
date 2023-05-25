42b44bc3e3780ac20601a6c901b0866b286705ca
root <nkaduba@gmail.com>
2023-05-25 03:55:21 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 """
 Script using REST API that returns info about a TODO list progress
-""" 
+"""
 import requests
 import sys
 
 if __name__ == "__main__":
     """URL without path"""
-    url = "https://jsonplaceholder.typicode.com/"
-    user_id = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = sys.argv[1]
     user_endpoint = "{}/users/{}".format(url, user_id)
-    name = requests.get(user_endpoint).json()
-    task_endpoint = "{}/todos".format(url, user_id)
+    name = requests.get(user_endpoint).json().get("name")
+    task_endpoint = "{}/todos".format(url)
     tasks = requests.get(task_endpoint).json()
-    all_tasks = [task for task in tasks if task.get("completed") is True]
-    done_tasks = [task.get("task") for task in tasks 
-                  if task.get("completed") is True]
+    all_tasks = [dict for dict in tasks if dict.get("userId") == user_id]
+    done_tasks = [dict for dict in all_tasks
+                  if dict.get("completed") == True]
 
     print("Employee {} is done with tasks({}/{}):"
-            .format(name.get("name"), len(all_tasks), len(tasks)))
-
-    for task in done_tasks:
-        print("\t {}".format(task))
+        .format(name, len(done_tasks), len(all_tasks)))
