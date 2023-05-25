501da36ffb5c25c3c9c1bdcc5fc05f9d1a026c6e
root <nkaduba@gmail.com>
2023-05-25 05:12:16 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
 
     comp_tasks = [t.get("title") for t in tasks if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):"
-        .format(user.get("name"), len(comp_tasks), len(tasks)))
+          .format(user.get("name"), len(comp_tasks), len(tasks)))
     [print("\t {}".format(c)) for c in comp_tasks]
