9642e3132f05ac44b95612b57cff3011dcfd8c8b
root <nkaduba@gmail.com>
2023-06-06 15:05:09 -0700

    api2 updated

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -16,26 +16,25 @@ def count_words(subreddit, word_list, array=None, after=""):
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
 
-    if response.status_code == 404:
-        return None
+    if response.status_code == 200:
+        result = response.json().get("data")
+        after = result.get("after")
 
-    result = response.json().get("data")
-    after = result.get("after")
-    for c in result.get("children"):
-        split = c.get("data").get("title").lower().split()
+        for c in result.get("children"):
+            split = c.get("data").get("title").lower().split()
 
-        for word in word_list:
-            if word.lower() in split:
-                number = len([n for n in split if n == word.lower()])
-                if array.get(word) is None:
-                    array[word] = number
-                else:
-                    array[word] += number
+            for word in word_list:
+                if word.lower() in split:
+                    number = len([n for n in split if n == word.lower()])
+                    if array.get(word) is None:
+                        array[word] = number
+                    else:
+                        array[word] += number
 
-    if after:
-        count_words(subreddit, word_list, array, after)
-    else:
-        result_count = sorted(array.items(), key=lambda x: (-x[1],
-                              x[0].lower()))
-        for word, count in result_count:
-            print("{}: {}".format(word.lower(), count))
+        if after:
+            count_words(subreddit, word_list, array, after)
+        else:
+            result_count = sorted(array.items(), key=lambda x: (-x[1],
+                                  x[0].lower()))
+            for word, count in result_count:
+                print("{}: {}".format(word.lower(), count))
