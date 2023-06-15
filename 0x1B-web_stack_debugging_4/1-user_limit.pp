8697e8002d30c0f2d3c13c85931532aa8bc07144
root <nkaduba@gmail.com>
2023-06-15 14:03:54 +0000

    Task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# Script to increase the number of login attempts by a user
+# change number of hard limit
+exec { 'change_hard_value_to_55':
+  command => "/bin/sed -i 's/5/55/g' /etc/security/limits.conf",
+}
+
+# change number of soft limit
+exec { 'change_soft_value_to_44':
+  command => "/bin/sed/ -i 's/4/44/g' /etc/security/limits.conf",
+}
