759af44d9f42ccce134450bec614407064a86b8b
Kaych3e <nkaduba@gmail.com>
2023-06-15 14:30:46 +0000

    Task1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -6,5 +6,5 @@ exec { 'change_hard_value_to_55':
 
 # change number of soft limit
 exec { 'change_soft_value_to_44':
-  command => "/bin/sed/ -i 's/4/44/g' /etc/security/limits.conf",
+  command => "/bin/sed -i 's/4/44/g' /etc/security/limits.conf",
 }
